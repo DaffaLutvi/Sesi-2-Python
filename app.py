69c62414d7205a1d27d111fd15030ce553a75d46
DaffaLutvi <daffalutfhi92@gmai.com>
2025-02-17 15:07:20 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+#name_1 , name_2, name_3 = "dudi","duda","didu"
+#x= y = z = 1
+
+first_name = "dudi"
+last_name = "setiawan"
+age = 19
+is_married= False
+amount_money = 10000000
+
+print (first_name ,last_name, age , is_married,amount_money)# Sesi-2-Python
